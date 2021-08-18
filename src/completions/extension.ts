188c264dcc8a81aab20dc37897fcb6448b06d6ae
Arman Dejbakhsh <arman.dejbakhsh@symphonyretailai.com>
2021-08-18 17:11:15 +0200

    Finally it works, that really was a pickle

--- src/completions/extension.ts
+++ src/completions/extension.ts
@@ -6,49 +6,25 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-  const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
+  const provider1 = vscode.languages.registerCompletionItemProvider({ language: 'GII'} , {
 
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
       // a simple completion item which inserts `Hello World!`
-      const simpleCompletion = new vscode.CompletionItem('Hello World!');
+      const simpleCompletion = new vscode.CompletionItem(vscode.env.appName);
+      simpleCompletion.insertText = JSON.stringify(context);
 
       // a completion item that inserts its text as snippet,
       // the `insertText`-property is a `SnippetString` which will be
       // honored by the editor.
-      const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-      snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-      snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
-      
-      const snippetC2 = new vscode.CompletionItem('GRAPH_TYPE');
-      snippetC2.insertText = new vscode.SnippetString("GRAPH_TYPE:${1|MSLine,MScolumn2D,MScombidy2D,angulargauge,bubble,column2D,line,msbar2D,mscombi2D,msstackedcolumn2D,msstackedcolumn2Dlinedy,pie2D,pie3D,pivot,stackedarea2D,stackedcolumn2Dline,table,text|}");
-      snippetC2.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
-
-
-
-      // a completion item that can be accepted by a commit character,
-      // the `commitCharacters`-property is set which means that the completion will
-      // be inserted and then the character will be typed.
-      const commitCharacterCompletion = new vscode.CompletionItem('console');
-      commitCharacterCompletion.commitCharacters = ['.'];
-      commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
-
-      // a completion item that retriggers IntelliSense when being accepted,
-      // the `command`-property is set which the editor will execute after 
-      // completion has been inserted. Also, the `insertText` is set so that 
-      // a space is inserted after `new`
-      const commandCompletion = new vscode.CompletionItem('new');
-      commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-      commandCompletion.insertText = 'new ';
-      commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+      const graphType = new vscode.CompletionItem('GRAPH_TYPE');
+      graphType.insertText = new vscode.SnippetString("GRAPH_TYPE:${1|MSLine,MScolumn2D,MScombidy2D,angulargauge,bubble,column2D,line,msbar2D,mscombi2D,msstackedcolumn2D,msstackedcolumn2Dlinedy,pie2D,pie3D,pivot,stackedarea2D,stackedcolumn2Dline,table,text|}");
+      graphType.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
 
       // return all completion items as array
       return [
-        snippetC2,
-        simpleCompletion,
-        snippetCompletion,
-        commitCharacterCompletion,
-        commandCompletion
+        graphType,
+        simpleCompletion
       ];
     }
   });
