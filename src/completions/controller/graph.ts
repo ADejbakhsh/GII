1c6d7b5a55038a5092e748e0b2ad1480e54740c6
Arman Dejbakhsh <arman.dejbakhsh@symphonyretailai.com>
2021-08-20 16:39:59 +0200

    Now also read expression_fct if it exist

--- src/completions/controller/graph.ts
+++ src/completions/controller/graph.ts
@@ -5,23 +5,36 @@ export class Graph {
 
   // return all the expression revelant to the context
   static async exp(document: vscode.TextDocument): Promise<Array<vscode.CompletionItem>> {
-    const expressionPath = path.parse(document.uri.fsPath).dir + '/expressions.txt';
-    const expression = await vscode.workspace.openTextDocument(expressionPath);
-    const text = expression.getText();
-    const listExpressions = text.match(/^[a-zA-Z_0-9]*=/mg) || [];
 
-    const array: Array<vscode.CompletionItem> = [];
+    const mapResult = ['/expressions.txt', '/expressions_fct.txt'].map(async (expressionFile): Promise<Array<vscode.CompletionItem>> => {
+      const expressionPath = path.parse(document.uri.fsPath).dir + expressionFile;
+      let expression;
+      try {
+        expression = await vscode.workspace.openTextDocument(expressionPath);
+      } catch (e) {
+        return [];
+      }
+      console.log(expressionPath, expression);
+      const text = expression.getText();
+      const listExpressions = text.match(/^[a-zA-Z_0-9]*=/mg) || [];
 
-    listExpressions.every((item) => {
-      // remove equal signe at the end of the string
-      const exp = item.substring(0, item.length - 1);
-      const completionItem = new vscode.CompletionItem(exp);
-      completionItem.kind = vscode.CompletionItemKind.Method;
+      const array: Array<vscode.CompletionItem> = [];
 
-      array.push(completionItem);
-      return true;
+      listExpressions.every((item) => {
+        // remove equal signe at the end of the string
+        const exp = item.substring(0, item.length - 1);
+        const completionItem = new vscode.CompletionItem(exp);
+        completionItem.kind = vscode.CompletionItemKind.Method;
+
+        array.push(completionItem);
+        return true;
+      });
+
+      return array;
     });
 
-    return array;
+    const result = await Promise.all(mapResult);
+
+    return result[0].concat(result[1]);
   }
 }
