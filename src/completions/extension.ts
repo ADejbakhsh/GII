315a05bc853b6adbc96d88a466156dffef6b7aba
Arman Dejbakhsh <arman.dejbakhsh@symphonyretailai.com>
2021-08-20 14:07:02 +0200

    prompte all the choices of the current folder

--- src/completions/extension.ts
+++ src/completions/extension.ts
@@ -1,18 +1,14 @@
 /*---------------------------------------------------------
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
-
+import path = require('path');
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-  const provider1 = vscode.languages.registerCompletionItemProvider({ language: 'GII'} , {
+  const provider1 = vscode.languages.registerCompletionItemProvider({ language: 'GII', pattern: '**/UNIVERSE_FACTORY/**' }, {
 
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
-      // a simple completion item which inserts `Hello World!`
-      const simpleCompletion = new vscode.CompletionItem(vscode.env.appName);
-      simpleCompletion.insertText = JSON.stringify(context);
 
       // a completion item that inserts its text as snippet,
       // the `insertText`-property is a `SnippetString` which will be
@@ -20,35 +16,49 @@ export function activate(context: vscode.ExtensionContext) {
       const graphType = new vscode.CompletionItem('GRAPH_TYPE');
       graphType.insertText = new vscode.SnippetString("GRAPH_TYPE:${1|MSLine,MScolumn2D,MScombidy2D,angulargauge,bubble,column2D,line,msbar2D,mscombi2D,msstackedcolumn2D,msstackedcolumn2Dlinedy,pie2D,pie3D,pivot,stackedarea2D,stackedcolumn2Dline,table,text|}");
       graphType.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
-
+      
       // return all completion items as array
       return [
-        graphType,
-        simpleCompletion
+        graphType
       ];
     }
   });
 
-  const provider2 = vscode.languages.registerCompletionItemProvider(
-    'plaintext',
-    {
-      provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+  const provider2 = vscode.languages.registerCompletionItemProvider({ language: 'GII', pattern: '**/UNIVERSE_FACTORY/**' }, {
+    async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+      const linePrefix = document.lineAt(position).text.substr(0, position.character);
+
+      if (/^GRAPH_EXP/.test(linePrefix)) {
+        console.log('provideCompletionItems');
+        const expressionPath = path.parse(document.uri.fsPath).dir + '/expressions.txt';
+        const expression = await vscode.workspace.openTextDocument(expressionPath);
+        const text = expression.getText();
+        const listExpressions = text.match(/^[a-zA-Z_0-9]*=/mg) || [];
+
+        const array: Array<vscode.CompletionItem> = [];
+        listExpressions.every((item) => {
+          array.push(new vscode.CompletionItem(item, vscode.CompletionItemKind.Method));
+          return true;
+        });
+
+        return array;
+
+      } else if (linePrefix.endsWith('console.')) {
 
         // get all text until the `position` and check if it reads `console.`
         // and if so then complete if `log`, `warn`, and `error`
-        const linePrefix = document.lineAt(position).text.substr(0, position.character);
-        if (!linePrefix.endsWith('console.')) {
-          return undefined;
-        }
-
-        return [
+        const array = [
           new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
           new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
           new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
         ];
+        console.log('array', array);
+        return array;
       }
-    },
-    '.' // triggered whenever a '.' is being typed
+
+      return undefined;
+    }
+  },
   );
 
   context.subscriptions.push(provider1, provider2);
